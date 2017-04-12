b9ccc1b599bba9cb6fde21152360de59aa77a143
mirza <mmabaig18@gmail.com>
2017-04-12 23:00:53 +0100

    product option list

--- src/app/pages/productOptionDetail/productOptionDetail.module.ts
+++ src/app/pages/productOptionDetail/productOptionDetail.module.ts
@@ -2,9 +2,7 @@ import { NgModule }      from '@angular/core';
 import { CommonModule }  from '@angular/common';
 import { FormsModule as AngularFormsModule } from '@angular/forms';
 import { NgaModule } from '../../theme/nga.module';
-
-import { BasicForm } from './basicForm/basicForm.component';
-import { HorizontalForm } from './horizontalForm/horizontalForm.component';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
 
 
 import { routing }       from './productOptionDetail.routing';
@@ -17,12 +15,11 @@ import {ProductOptionDeail} from "./productOptionDetail.component";
     CommonModule,
     AngularFormsModule,
     NgaModule,
-    routing
+    routing,
+    Ng2SmartTableModule
   ],
   declarations: [
-    ProductOptionDeail,
-    BasicForm,
-    HorizontalForm
+    ProductOptionDeail
   ]
 })
 export class ProductOptionDetailModule {
